8d4a8d51eb92179847fc28216f8a820c41425475
chris <chri402s@stud.kea.dk>
2023-03-28 13:25:20 +0200

    small correction

--- Program.cs
+++ Program.cs
@@ -15,18 +15,18 @@ class FileReader {
 
             using (StreamReader reader = new StreamReader(file)) {
                 while (!reader.EndOfStream) {
-                i += 1;
-                
-                var line = reader.ReadLine();
-                Console.WriteLine(line);
-                Console.WriteLine(i);
-                string[] values = line.Split(',');
+                    i += 1;
+                    
+                    var line = reader.ReadLine();
+                    Console.WriteLine(line);
+                    Console.WriteLine(i);
+                    string[] values = line.Split(',');
 
-                Game tuple = new Game(values[0], values[1], int.Parse(values[2]), int.Parse(values[3]));
-                gameRounds.Add(tuple);
-                
+                    Game tuple = new Game(values[0], values[1], int.Parse(values[2]), int.Parse(values[3]));
+                    gameRounds.Add(tuple);
+                    
+                }
             }
-        }
         }
 
         foreach (Game g in gameRounds) {
