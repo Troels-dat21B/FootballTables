75e89cd4e698c8787062058a69b0eaae21e97492
Lasse-R <lass531n@stud.kea.dk>
2023-03-28 13:17:47 +0200

    csv filer rettet + csv reader funktionel

--- Program.cs
+++ Program.cs
@@ -10,20 +10,23 @@ class FileReader {
     static void Main() {
 
         List<Game> gameRounds = new List<Game>();
+        var i = 0;
+        foreach (string file in Directory.EnumerateFiles("./csv filer/Rounds", "*.csv")) {
 
-            foreach (string file in Directory.EnumerateFiles("./csv filer/Rounds", "*.csv")) {
+            using (StreamReader reader = new StreamReader(file)) {
+                while (!reader.EndOfStream) {
+                i += 1;
+                
+                var line = reader.ReadLine();
+                Console.WriteLine(line);
+                Console.WriteLine(i);
+                string[] values = line.Split(',');
 
-                using (StreamReader reader = new StreamReader(file)) {
-                    while (!reader.EndOfStream) {
-                    
-                    var line = reader.ReadLine();
-                    string[] values = line.Split(',');
-    
-                    Game tuple = new Game(values[0], values[1], int.Parse(values[2]), int.Parse(values[3]));
-                    gameRounds.Add(tuple);
-                    
-                }
+                Game tuple = new Game(values[0], values[1], int.Parse(values[2]), int.Parse(values[3]));
+                gameRounds.Add(tuple);
+                
             }
+        }
         }
 
         foreach (Game g in gameRounds) {
