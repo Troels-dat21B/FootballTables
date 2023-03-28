0799fa026fdd1bc793dd457557c0876b869d6fcf
Lasse-R <lass531n@stud.kea.dk>
2023-03-28 14:57:23 +0200

    sikkerheds commit

--- Program.cs
+++ Program.cs
@@ -1,37 +1,35 @@
 ﻿using System;
 using System.IO;
-using System.Collections.Generic;
 using System.Linq;
-using System.Globalization;
-using CsvHelper;
 namespace Game;
 
 class FileReader {
     static void Main() {
 
-        List<Game> gameRounds = new List<Game>();
-        var i = 0;
+        csvAllRoundsReader();
+    }
+
+    public static List<Game> csvAllRoundsReader() {
+
+        List<Game> allGameRounds = new List<Game>();
         foreach (string file in Directory.EnumerateFiles("./csv filer/Rounds", "*.csv")) {
 
             using (StreamReader reader = new StreamReader(file)) {
+
                 while (!reader.EndOfStream) {
-                i += 1;
                 
                 var line = reader.ReadLine();
-                Console.WriteLine(line);
-                Console.WriteLine(i);
                 string[] values = line.Split(',');
 
                 Game tuple = new Game(values[0], values[1], int.Parse(values[2]), int.Parse(values[3]));
-                gameRounds.Add(tuple);
+                allGameRounds.Add(tuple);
                 
             }
         }
-        }
-
-        foreach (Game g in gameRounds) {
-
-            Console.WriteLine(g.roundInfo(g));
-        }
     }
+
+    return allGameRounds;
+
+    }
+
 }
