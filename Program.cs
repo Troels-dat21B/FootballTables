8ae86e5fd8d5dbeb94f74a66a597e8ad7e77ddf7
Troels-dat21B <troe1912@dat.stud.kea>
2023-03-28 15:10:04 +0200

    t

--- Program.cs
+++ Program.cs
@@ -9,16 +9,13 @@ class FileReader {
     static void Main() {
 
         List<Game> gameRounds = new List<Game>();
-        var i = 0;
+
         foreach (string file in Directory.EnumerateFiles("./csv filer/Rounds", "*.csv")) {
 
             using (StreamReader reader = new StreamReader(file)) {
                 while (!reader.EndOfStream) {
-                    i += 1;
                     
                     var line = reader.ReadLine();
-                    Console.WriteLine(line);
-                    Console.WriteLine(i);
                     string[] values = line.Split(',');
 
                     Game tuple = new Game(values[0], values[1], int.Parse(values[2]), int.Parse(values[3]));
